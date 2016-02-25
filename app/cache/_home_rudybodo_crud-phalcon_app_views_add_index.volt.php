b7e365dcc41631d6b8e4de6bb0558c4f426fb60a
rudybodo <transkumba@gmail.com>
2016-02-25 16:10:21 +0800

    push to github

--- app/cache/_home_rudybodo_crud-phalcon_app_views_add_index.volt.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>Add Member</title>
-</head>
-<body>
-<p> Test Add Member
-</body>
-</html>
--- app/cache/_home_rudybodo_crud-phalcon_app_views_index_index.volt.php
+++
@@ -1,4 +0,0 @@
-<div class="page-header">
-    <h1>CRUD Member</h1>
-    <a href="/member">App</a>
-</div>
