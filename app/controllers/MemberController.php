b59a5f7f59d679ff9c5434eab8fbaf72e57a15c5
rudybodo <transkumba@gmail.com>
2016-02-27 01:38:00 +0800

    push to github

--- app/controllers/MemberController.php
+++ app/controllers/MemberController.php
@@ -1,38 +1,103 @@
 <?php
 
-class MemberController extends ControllerBase
-{
+class MemberController extends ControllerBase {
 
-  public function indexAction()
-  {
-
-    $this->view->data = Member::find();
-  }
-
-
-  public function addAction()
-  {
-
-    $member = new Member();
-    $member ->name = $this ->request ->getPost("Nama");
-    $member ->address = $this ->request ->getPost("Address");
-    $member ->email = $this ->request ->getPost("Email");
-    $member ->phonenumber = $this ->request ->getPost("Phonenumber");
-
-    if ($member->save() == false) {
-      foreach ($member->getMessages() as $e){
-      echo e;
-      }
+    public function indexAction() {
     }
-}
 
-  public function editAction()
-  {
+    public function createAction() {
 
-  }
+        // we assume every post are there
+        // otherwise it's error
+        // or add validation later
+        $post = $this->request->getPost();
 
-  public function deleteAction()
-  {
+        $member = new Member();
 
-  }
+        if (!$member->create($post)) {
+            $this->view->errors = [];
+            foreach ($member->getMessages() as $err)
+                array_push($this->view->errors, [
+                    "field"     => $err->getField(),
+                    "message"   => $err->getMessage()
+                  ]);
+        }
+
+        $this->view->data = $member;
+        $this->view->msg = "Success";
+    }
+
+    public function readAction() {
+
+        // when query string "id" exist
+        // means we read a single data
+        // otherwise reading all data
+        $id = $this->request->getQuery("id");
+        if ($id) {
+            $data = Member::findFirstById($id);
+            $this->view->id = $id;
+        } else {
+            $data = Member::find();
+        }
+
+        // if no records found
+        if (!$data) {
+            $this->view->error = "Record Not Found";
+            return;
+        }
+
+        $this->view->data = $data;
+        $this->view->msg = "Success";
+
+    }
+
+    public function updateAction() {
+
+        $id = $this->request->getQuery("id");
+        if (!$id) {
+            $this->view->error = "Id Required";
+            return;
+        }
+
+        $member = Member::findFirstById($id);
+        if (!$member) {
+            $this->view->error = "Member Not Found";
+            return;
+        }
+
+        $post = $this->request->getPost();
+        if (!$member->update($post)) {
+            $this->view->errors = [];
+            foreach ($member->getMessage() as $err)
+                array_push($this->view->errors, [
+                    "field"     => $err->getField(),
+                    "message"   => $err->getMessage(),
+                    ]);
+            return;
+        }
+
+        $this->view->data = $member;
+        $this->view->msg = "Success";
+
+    }
+
+    public function deleteAction() {
+
+        $id = $this->request->getQuery("id");
+        if (!$id) {
+            $this->view->error = "Id Required";
+            return;
+        }
+
+        $member = Member::findFirstById($id);
+        if (!$member) {
+            $this->view->error = "Member Not Found";
+            return;
+        }
+
+        $member->delete();
+
+        $this->view->msg = "Success";
+    }
+
 }
---
+++ app/views/member/create.volt
@@ -0,0 +1,20 @@
+<div><h1>Add Data</h1></div>
+<form role="form">
+  <div class="form-group">
+    <label for="Name">Name </label>
+    <input type="text" name="name" class="form-control" id="text">
+  </div>
+  <div class="form-group">
+    <label for="Name">Address </label>
+    <input type="text" name="address" class="form-control" id="text">
+  </div>
+  <div class="form-group">
+      <label for="email">Email address:</label>
+      <input type="email" name="email" class="form-control" id="email">
+  </div>
+  <div class="form-group">
+    <label for="Name">Phone Number </label>
+    <input type="text" name="phonenumber" class="form-control" id="text">
+  </div>
+  <button type="submit" class="btn btn-default" value="add">Add</button>
+</form>
